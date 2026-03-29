ad8d9d2b8d59fd0c236aa6d44186e5569172e50a
Seikh Souvagya Mustakim <seikhsouvagyamustakim@gmail.com>
2026-03-29 12:11:29 +0530

    Add window controls, fix multi-mode layout, and configure startup

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1440,
     height: 900,
-    frame: false,          // no title bar = wallpaper feel
-    fullscreen: false,     // set true if you want fullscreen on launch
+    frame: false,
+    fullscreen: true,
+    autoHideMenuBar: true,
     backgroundColor: '#0a0a0f',
     webPreferences: {
-      nodeIntegration: false
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
   win.loadFile('index.html')
+
+  ipcMain.on('minimize-app', () => {
+    win.minimize();
+  })
+
+  ipcMain.on('close-app', () => {
+    app.quit();
+  })
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
+
+  // Make the app launch automatically when Windows boots up
+  app.setLoginItemSettings({
+    openAtLogin: true,
+    openAsHidden: false, // Start normally
+    path: app.getPath('exe') // Use the built executable
+  })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
